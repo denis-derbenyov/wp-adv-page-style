f04b5c67d816b3498ca494ddb43fc4b8418a4660
Denis Derbenyov <denis.derbenyov@wdg.com.ua>
2011-11-25 16:45:59 +0200

    create plugin info

---
+++ wp-adv-page-style.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Advanced Page Style
+Plugin URI: http://newage.pc.org.ua/wpaps
+Description: A brief description of the Plugin.
+Version: The Plugin's Version Number, e.g.: 1.0
+Author: Denis Derbenyov
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+?>
